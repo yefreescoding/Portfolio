7bf8a2d111b10d2f6167a692b15745ccbad70c3b
yefreescoding <yefree13santos@outlook.com>
2023-06-05 17:16:37 +0200

    about me section done, new fonts added

--- portfolio2.0/src/Data/aboutMeData.js
+++ portfolio2.0/src/Data/aboutMeData.js
@@ -1,22 +1,12 @@
 const aboutMe = [
   {
-    para: `Hey! I am a designer, Front End developer, and illustrator. I love
-        working on the web and making sure it continues to be a place for
-        everyone and our creative, personal, and weird passions.`,
+    para: `¡Eh! Soy diseñador, desarrollador Front End e ilustrador. Me encanta Trabajar en la web y asegurarse de que siga siendo un lugar para todos y nuestras pasiones creativas, personales y extrañas.`,
   },
   {
-    para: `I,ve learning how to code since 2022 and take it serious since February
-        of 2023. You can see my evolution in this field trough my projects, my
-        blog and social media accounts. So far I've got plenty understand of
-        HTML, SEO, semantic tags and how to make webpages available for
-        everybody.`,
+    para: `He aprendido a codificar desde 2022 y lo tomo en serio desde febrero de 2023. Puedes ver mi evolución en este campo a través de mis proyectos, mi blog y cuentas de redes sociales. Hasta ahora tengo mucha comprensión de HTML, SEO, etiquetas semánticas y cómo hacer que las páginas web estén disponibles para todos.`,
   },
   {
-    para: `I believe in my abilities and knowledge would bring a lot of value to
-        any business that want to add me to their workforce , knowing that I've
-        got to keep learning and there's a lot of stuff, like libraries, API and
-        documentation that I must need to add in my resume to become a great
-        full stack developer.`,
+    para: `Creo que mis habilidades y conocimientos aportarían mucho valor a cualquier empresa que quiera agregarme a su fuerza laboral, sabiendo que tengo Tengo que seguir aprendiendo y hay muchas cosas, como bibliotecas, API y documentación que debo agregar en mi currículum para convertirme en un gran Desarrollador full stack.`,
   },
 ];
 
