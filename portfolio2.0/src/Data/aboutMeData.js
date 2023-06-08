b49300f97c79a9b57e22f96fa9039d54ee1b7b04
Yefree Valdez <yefree13santos@outlook.com>
2023-06-08 14:36:32 +0200

    commiting before adding astromap

--- portfolio2.0/src/Data/aboutMeData.js
+++ portfolio2.0/src/Data/aboutMeData.js
@@ -1,6 +1,6 @@
 const aboutMe = [
   {
-    para: `¡Eh! Soy diseñador, desarrollador Front End e ilustrador. Me encanta Trabajar en la web y asegurarse de que siga siendo un lugar para todos y nuestras pasiones creativas, personales y extrañas.`,
+    para: `¡Ey! Soy desarrollador Front End, diseñador e ilustrador. Me encanta Trabajar en la web y asegurarse de que siga siendo un lugar para todos y nuestras pasiones creativas, personales y extrañas.`,
   },
   {
     para: `He aprendido a codificar desde 2022 y lo tomo en serio desde febrero de 2023. Puedes ver mi evolución en este campo a través de mis proyectos, mi blog y cuentas de redes sociales. Hasta ahora tengo mucha comprensión de HTML, SEO, etiquetas semánticas y cómo hacer que las páginas web estén disponibles para todos.`,
