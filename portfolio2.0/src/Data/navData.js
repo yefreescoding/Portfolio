6330fc3d1a139fb6680e937875941bce28345038
Yefree Valdez <yefree13santos@outlook.com>
2023-06-27 13:07:09 +0200

    new ui design cover. Looking really good.

--- portfolio2.0/src/Data/navData.js
+++ portfolio2.0/src/Data/navData.js
@@ -1,15 +1,16 @@
 const navData = [
   {
-    name: 'source code',
+    name: 'GitHub',
     link: 'https://github.com/yefreescoding/Portfolio.git',
+  },
+
+  {
+    name: 'proyectos',
+    link: '#projects',
   },
   {
     name: 'blog',
     link: 'https://you-me-blog.vercel.app/',
-  },
-  {
-    name: 'curriculum',
-    link: '/cv/CV-Yefree-2023.pdf',
   },
 ];
 export default navData;
