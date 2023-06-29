cc8f0b3d23a0e1eff9866cd0bc17896501bfc4eb
Yefree Valdez <yefree13santos@outlook.com>
2023-06-29 12:11:51 +0200

    new ui about me styled

--- portfolio2.0/src/Data/skillsData.js
+++ portfolio2.0/src/Data/skillsData.js
@@ -1,48 +1,49 @@
 const skills = [
   {
-    image: '/images/icons/html.png',
+    image: '/images/icons/html-5.avif',
     name: 'HTML',
-    description: `Paginas HTML organizadas, limpias y accesibles.`,
+    description: `No mucho que añadir.`,
   },
   {
-    image: '/images/icons/css-line.png',
+    image: '/images/icons/css-3.avif',
     name: 'CSS',
-    description: `Desde cero, con SASS, y también Tailwind.`,
+    description: `(SASS & Tailwind).`,
   },
   {
-    image: '/images/icons/javascript.png',
+    image: '/images/icons/js.avif',
     name: 'JavaScript',
-    description: `Vanilla Javascript es lo mejor, nadie me hará cambiar de opinion.`,
+    description: `Vanilla Javascript.`,
   },
   {
     image: '/images/icons/favicon.svg',
     name: 'Astro',
-    description: `De lo mejorcito que hay en cuanto a frameworks a dia de hoy.`,
+    description: `Creación paginas estáticas 100%.`,
   },
   {
-    image: '/images/icons/react.png',
+    image: '/images/icons/react.avif',
     name: 'ReactJS',
-    description: `De lo mejorcito que hay en cuanto a frameworks a dia de hoy.`,
+    description: `el líder indiscutible`,
   },
   {
-    image: '/images/icons/react.png',
+    image: '/images/icons/nodejs.avif',
     name: 'NodeJS',
-    description: `De lo mejorcito que hay en cuanto a frameworks a dia de hoy.`,
+    description: `Para mi backend`,
   },
   {
-    image: '/images/icons/react.png',
+    image: '/images/icons/mysql.avif',
     name: 'MySQL',
-    description: `De lo mejorcito que hay en cuanto a frameworks a dia de hoy.`,
+    description: `Bases de datos`,
   },
   {
-    image: '/images/icons/react.png',
+    image: '/images/icons/php.avif',
     name: 'PHP',
-    description: `De lo mejorcito que hay en cuanto a frameworks a dia de hoy.`,
+    description: `Aun se utiliza.`,
   },
   {
-    image: '/images/icons/react.png',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/f/f1/Vitejs-logo.svg',
     name: 'ViteJS',
-    description: `De lo mejorcito que hay en cuanto a frameworks a dia de hoy.`,
+    description: `Speed x2.`,
   },
 ];
 export default skills;
