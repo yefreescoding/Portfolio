f3e9b0d35393085bae45d56488072332b146316e
Yefree Valdez <yefree13santos@outlook.com>
2023-07-12 11:43:30 +0200

    added vercel analitycs

--- portfolio2.0/astro.config.mjs
+++ portfolio2.0/astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://portfolioyefreevaldez.vercel.app',
-  integrations: [sitemap(), tailwind()]
+  integrations: [sitemap(), tailwind()],
+  output: 'static',
+  adapter: vercel({
+    analytics: true,
+  }),
 });
