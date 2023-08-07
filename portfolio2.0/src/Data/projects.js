ee354757a182a4c33d91e7a2cfdcac8c6a8f18bb
Yefree Valdez <yefree13santos@outlook.com>
2023-08-07 17:44:27 +0200

    refactor the code

---
+++ portfolio2.0/src/Data/projects.js
@@ -0,0 +1,22 @@
+export const projects = [
+  {
+    id: 1,
+    name: '30 proyectos 30 días',
+    description:
+      'Repositorio de Github de un reto que me he propuesto a finales del mes de Julio. 30 proyectos en 30 dias, uno por dia. ¡No puedo dejar pasar ni uno solo!',
+    link: 'https://yefreescoding.github.io/30-projects-30-days/',
+    github: 'https://github.com/yefreescoding/30-projects-30-days.git',
+    imagesrc: '/images/projects-img/twitter-post-git.jpg',
+    position: false,
+  },
+  {
+    id: 2,
+    name: 'music-gram',
+    description:
+      'Una pagina web donde poder ver los artistas del momento, sus ultimas canciones, colaboraciones y proximos consierto. Creado con ASTRO, y utilizando los fundamentos basicos de HTML, CSS y JavaScript.',
+    link: 'https://music-gram-yefreescoding.vercel.app',
+    github: 'https://github.com/yefreescoding/music-gram.git',
+    imagesrc: '/images/projects-img/music-gram.webp',
+    position: true,
+  },
+];
