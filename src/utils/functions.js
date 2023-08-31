8046a47bb0a2b28700d1b8fc38a5682753584168
Yefree Valdez <yefree13santos@outlook.com>
2023-08-31 18:54:23 +0200

    fix: bug prettier add settings file

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,16 +1,16 @@
 export function formatDate(date) {
-  return new Date(date).toLocaleDateString('en-US', {
-    timeZone: 'UTC',
+  return new Date(date).toLocaleDateString("en-US", {
+    timeZone: "UTC",
   });
 }
 
 export function formatDateMonth(date) {
   const options = {
-    timeZone: 'UTC',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
+    timeZone: "UTC",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
   };
 
-  return new Date(date).toLocaleDateString('en-US', options);
+  return new Date(date).toLocaleDateString("en-US", options);
 }
