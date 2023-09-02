adc9c4ecf4231b4e16f96a6221f793c23b99652d
Yefree Valdez <yefree13santos@outlook.com>
2023-09-02 13:03:37 +0200

    feat: dark mode btn style

--- src/Data/navData.js
+++ src/Data/navData.js
@@ -1,23 +1,23 @@
 const navData = [
   {
-    name: 'Sobre mi',
-    link: '/about/',
+    name: "proyectos",
+    link: "/#projects",
   },
   {
-    name: 'proyectos',
-    link: '/#projects',
+    name: "Sobre mi",
+    link: "/about/",
   },
   {
-    name: 'mini blog',
-    link: '/memorias/',
+    name: "mini blog",
+    link: "/memorias/",
   },
   {
-    name: 'GitHub',
-    link: 'https://github.com/yefreescoding/',
+    name: "GitHub",
+    link: "https://github.com/yefreescoding/",
   },
   {
-    name: 'blog',
-    link: 'https://devlog.vercel.app/',
+    name: "blog",
+    link: "https://devlog.vercel.app/",
   },
 ];
 export default navData;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,16 +1,15 @@
-const toggleMobile = document.querySelector('#toggle-mobile-menu');
+const toggleMobile = document.querySelector("#toggle-mobile-menu");
 
-const navBarMobile = document.querySelector('.nav');
-// const imgBarMobile = document.querySelector('.nav');
+const navBarMobile = document.querySelector(".nav");
 
-toggleMobile.addEventListener('click', function () {
-  const visible = navBarMobile.getAttribute('data-visible');
+toggleMobile.addEventListener("click", function () {
+  const visible = navBarMobile.getAttribute("data-visible");
 
-  if (visible === 'false') {
-    navBarMobile.setAttribute('data-visible', true);
+  if (visible === "false") {
+    navBarMobile.setAttribute("data-visible", true);
     // toggleMobile.setAttribute('data-dark', true);
   } else {
-    navBarMobile.setAttribute('data-visible', false);
+    navBarMobile.setAttribute("data-visible", false);
     // toggleMobile.setAttribute('data-dark', false);
   }
 });
@@ -18,22 +17,22 @@ toggleMobile.addEventListener('click', function () {
 // =============================
 // =============================
 
-const darkModeBtn = document.querySelector('#dark-light');
-const darkModeBtnText = document.querySelector('#mode');
+// const darkModeBtn = document.querySelector('#dark-light');
+// const darkModeBtnText = document.querySelector('#mode');
 
-darkModeBtn.addEventListener('click', () => {
-  const mode = darkModeBtnText.getAttribute('data-mode');
+// darkModeBtn.addEventListener('click', () => {
+//   const mode = darkModeBtnText.getAttribute('data-mode');
 
-  if (mode === 'light') {
-    darkModeBtnText.setAttribute('data-mode', 'dark');
-    darkModeBtnText.textContent = 'dark';
-  } else {
-    darkModeBtnText.setAttribute('data-mode', 'light');
-    darkModeBtnText.textContent = 'light';
-  }
+//   if (mode === 'light') {
+//     darkModeBtnText.setAttribute('data-mode', 'dark');
+//     darkModeBtnText.textContent = 'dark';
+//   } else {
+//     darkModeBtnText.setAttribute('data-mode', 'light');
+//     darkModeBtnText.textContent = 'light';
+//   }
 
-  document.body.classList.toggle('dark-mode');
-});
+//   document.body.classList.toggle('dark-mode');
+// });
 
 // =============================
 // =============================
