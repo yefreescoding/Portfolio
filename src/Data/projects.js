3ac590596dc2575adf81897e6603f1b3a087995d
Yefree Valdez <yefree13santos@outlook.com>
2023-09-04 11:43:07 +0200

    feat: new style project grid

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,48 +1,56 @@
 export const projects = [
   {
     id: 1,
-    name: '30 proyectos 30 días',
+    name: "30 proyectos 30 días",
     description:
-      'Repositorio de Github de un reto que me he propuesto a finales del mes de Julio. 30 proyectos en 30 dias, uno por dia. ¡No puedo dejar pasar ni uno solo!',
-    link: 'https://yefreescoding.github.io/30-projects-30-days/',
-    github: 'https://github.com/yefreescoding/30-projects-30-days.git',
-    imagesrc: '/images/projects-img/30-30.jpg',
+      "Repositorio de Github de un reto que me he propuesto a finales del mes de Julio. 30 proyectos en 30 dias, uno por dia. ¡No puedo dejar pasar ni uno solo!",
+    link: "https://yefreescoding.github.io/30-projects-30-days/",
+    github: "https://github.com/yefreescoding/30-projects-30-days.git",
+    imagesrc: "/images/projects-img/30-30.jpg",
   },
   {
     id: 2,
-    name: 'Music-gram',
+    name: "Music-gram",
     description:
-      'Una pagina web donde poder ver los artistas del momento, sus ultimas canciones, colaboraciones y próximos conciertos. Creado con ASTRO, y utilizando los fundamentos basicos de HTML, CSS y JavaScript.',
-    link: 'https://music-gram-yefreescoding.vercel.app',
-    github: 'https://github.com/yefreescoding/music-gram.git',
-    imagesrc: '/images/projects-img/musicgram.jpg',
+      "Una pagina web donde poder ver los artistas del momento, sus ultimas canciones, colaboraciones y próximos conciertos. Creado con ASTRO, y utilizando los fundamentos basicos de HTML, CSS y JavaScript.",
+    link: "https://music-gram-yefreescoding.vercel.app",
+    github: "https://github.com/yefreescoding/music-gram.git",
+    imagesrc: "/images/projects-img/musicgram.jpg",
   },
   {
     id: 3,
-    name: 'Overthink',
+    name: "Overthink",
     description:
-      'Cuando te sientas lleno de ideas o pensamientos, déjalos salir. Esta app te ayuda a escribir y guardar esas ideas.',
-    link: 'https://overthink.vercel.app',
+      "Cuando te sientas lleno de ideas o pensamientos, déjalos salir. Esta app te ayuda a escribir y guardar esas ideas.",
+    link: "https://overthink.vercel.app",
     github:
-      'https://github.com/yefreescoding/30-projects-30-days/tree/main/project-19/overthinking',
-    imagesrc: '/images/projects-img/overthink.jpg',
+      "https://github.com/yefreescoding/30-projects-30-days/tree/main/project-19/overthinking",
+    imagesrc: "/images/projects-img/overthink.jpg",
   },
   {
     id: 4,
-    name: 'Resetéame',
+    name: "Resetéame",
     description:
-      'Archivos resets CSS, y HTML. Próximamente incluiré mas para diferentes frameworks, y workflows.',
-    link: 'https://reseteame.vercel.app/',
-    github: 'https://github.com/yefreescoding/reseteame',
-    imagesrc: '/images/projects-img/reseteame.jpg',
+      "Archivos resets CSS, y HTML. Próximamente incluiré mas para diferentes frameworks, y workflows.",
+    link: "https://reseteame.vercel.app/",
+    github: "https://github.com/yefreescoding/reseteame",
+    imagesrc: "/images/projects-img/reseteame.jpg",
   },
-  // {
-  //   id: 5,
-  //   name: 'Boxing Match',
-  //   description:
-  //     'Noticias del boxeo, las mejores peleas, los mejores boxeadores, la información que te interesa cortita y al pie. ',
-  //   link: 'https://proyecto-final-colon.000webhostapp.com/index.php',
-  //   github: 'https://github.com/yefreescoding/final-project-course',
-  //   imagesrc: '/images/projects-img/boxing.jpg',
-  // },
+  {
+    id: 5,
+    name: "Boxing Match",
+    description:
+      "Noticias del boxeo, las mejores peleas, los mejores boxeadores, la información que te interesa cortita y al pie. ",
+    link: "https://proyecto-final-colon.000webhostapp.com/index.php",
+    github: "https://github.com/yefreescoding/final-project-course",
+    imagesrc: "/images/projects-img/boxing.jpg",
+  },
+  {
+    id: 6,
+    name: "Nike Lite",
+    description: "",
+    link: "https://nike-lite.vercel.app",
+    github: "https://github.com/yefreescoding/nike-lite.git",
+    imagesrc: "/images/projects-img/nike-lite.jpg",
+  },
 ];
