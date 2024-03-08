406bf96030ffce091f5feec7e7ac1bbdb1ddae6d
Yefree Valdez <yefree13santos@outlook.com>
2024-03-08 13:33:40 +0100

    update: add new projects image/video and update links

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,12 +1,43 @@
 export const projects = [
   {
-    id: 1,
-    name: "30 proyectos 30 días",
+    id: 3,
+    name: "Overthink v2",
     description:
-      "Repositorio de Github. 30 proyectos en 30 dias, uno por dia. ¡No puedo dejar pasar ni uno solo!",
+      "React + typeScript application, no framework was used in the development of this website.",
+    link: "https://overthink-v-2.vercel.app",
+    github: "https://github.com/yefreescoding/overthink",
+    imagesrc: "/images/projects-img/overthink-v2.mp4",
+    video: true,
+  },
+  {
+    id: 7,
+    name: "React component. Horizontal scrolling.",
+    description:
+      "React + typeScript application, no framework was used in the development of this website.",
+    link: "https://horizontal-scrolling-react.vercel.app",
+    github: "https://github.com/yefreescoding/horizontal-scrolling-react",
+    imagesrc: "/images/projects-img/scroll-react.mp4",
+    video: true,
+  },
+  {
+    id: 4,
+    name: "Resetéame",
+    description:
+      "Archivos resets CSS, y HTML. Próximamente incluiré mas para diferentes frameworks, y workflows.",
+    link: "https://reseteame-4dackzyx3-yefreescoding.vercel.app",
+    github: "https://github.com/yefreescoding/reseteame",
+    imagesrc: "/images/projects-img/reseteame-v2.jpg",
+    video: false,
+  },
+  {
+    id: 1,
+    name: "30 proyectos 30 días.",
+    description:
+      "Repositorio de Github. 30 proyectos en 30 días, uno por dia. ¡No puedo dejar pasar ni uno solo!",
     link: "https://yefreescoding.github.io/30-projects-30-days/",
     github: "https://github.com/yefreescoding/30-projects-30-days.git",
     imagesrc: "/images/projects-img/30-30.jpg",
+    video: false,
   },
   {
     id: 2,
@@ -16,25 +47,7 @@ export const projects = [
     link: "https://music-gram-yefreescoding.vercel.app",
     github: "https://github.com/yefreescoding/music-gram.git",
     imagesrc: "/images/projects-img/musicgram.jpg",
-  },
-  {
-    id: 3,
-    name: "Overthink",
-    description:
-      "Cuando te sientas lleno de ideas o pensamientos, déjalos salir. Esta app te ayuda a escribir y guardar esas ideas.",
-    link: "https://overthink.vercel.app",
-    github:
-      "https://github.com/yefreescoding/30-projects-30-days/tree/main/project-19/overthinking",
-    imagesrc: "/images/projects-img/overthink.jpg",
-  },
-  {
-    id: 4,
-    name: "Resetéame",
-    description:
-      "Archivos resets CSS, y HTML. Próximamente incluiré mas para diferentes frameworks, y workflows.",
-    link: "https://reseteame.vercel.app/",
-    github: "https://github.com/yefreescoding/reseteame",
-    imagesrc: "/images/projects-img/reseteame.jpg",
+    video: false,
   },
   {
     id: 5,
@@ -44,6 +57,7 @@ export const projects = [
     link: "https://proyecto-final-colon.000webhostapp.com/index.php",
     github: "https://github.com/yefreescoding/final-project-course",
     imagesrc: "/images/projects-img/boxing.jpg",
+    video: false,
   },
   {
     id: 6,
@@ -53,5 +67,6 @@ export const projects = [
     link: "https://nike-lite.vercel.app",
     github: "https://github.com/yefreescoding/nike-lite.git",
     imagesrc: "/images/projects-img/nike-lite.jpg",
+    video: false,
   },
 ];
