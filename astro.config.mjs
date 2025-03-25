fec1d21649f1b53be27d7af4ff081e580a30baf2
Yefree Valdez <yefree13santos@outlook.com>
2025-03-25 10:39:59 -0400

    upgrade packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
 
 // https://astro.build/config
