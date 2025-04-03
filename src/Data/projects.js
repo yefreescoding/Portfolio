ef58068715f6a71dab86924dc8826ef2c006d7f4
Yefree Valdez <yefree13santos@outlook.com>
2025-04-02 22:53:36 -0400

    convert media

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -4,9 +4,9 @@ export const projects = [
     name: "Banking Landing Page",
     description:
       "Easy to use landing page for your banking website. No framework was used in the development of this website.",
-    link: "/",
-    github: "/",
-    imagesrc: "/images/projects-img/hero-video-1.mov",
+    link: "/memorias/",
+    github: "/memorias/",
+    imagesrc: "/images/projects/video/banking-landing-page.webm",
     video: true,
   },
   {
@@ -14,9 +14,9 @@ export const projects = [
     name: "Modern Concept Hero",
     description:
       "Easy to use landing page for your banking website. No framework was used in the development of this website.",
-    link: "/",
-    github: "/",
-    imagesrc: "/images/projects-img/hero-video-3.mov",
+    link: "/memorias/",
+    github: "/memorias/",
+    imagesrc: "/images/projects/video/modern-hero.webm",
     video: true,
   },
   {
@@ -24,9 +24,19 @@ export const projects = [
     name: "Overthink v2",
     description:
       "React + typeScript application, no framework was used in the development of this website.",
-    link: "https://overthink-v-2.vercel.app",
-    github: "https://github.com/yefreescoding/overthink",
-    imagesrc: "/images/projects-img/overthink-v2.mp4",
+    link: "/memorias/",
+    github: "/memorias/",
+    imagesrc: "/images/projects/video/overthink-v2.webm",
+    video: true,
+  },
+  {
+    id: 1009,
+    name: "Music Player Component",
+    description:
+      "React + typeScript application, no framework was used in the development of this website.",
+    link: "/memorias/",
+    github: "/memorias/",
+    imagesrc: "/images/projects/video/music-player-component.webm",
     video: true,
   },
   {
@@ -34,59 +44,19 @@ export const projects = [
     name: "Horizontal scrolling.",
     description:
       "React + typeScript application, no framework was used in the development of this website.",
-    link: "https://horizontal-scrolling-react.vercel.app",
-    github: "https://github.com/yefreescoding/horizontal-scrolling-react",
-    imagesrc: "/images/projects-img/scroll-react.mp4",
+    link: "/memorias/",
+    github: "/memorias/",
+    imagesrc: "/images/projects/video/scroll-react.webm",
     video: true,
   },
   {
-    id: 4,
-    name: "Resetéame",
+    id: 79087,
+    name: "Mail Component",
     description:
-      "Archivos resets CSS, y HTML. Próximamente incluiré mas para diferentes frameworks, y workflows.",
-    link: "https://reseteame-4dackzyx3-yefreescoding.vercel.app",
-    github: "https://github.com/yefreescoding/reseteame",
-    imagesrc: "/images/projects-img/reseteame-v2.jpg",
-    video: false,
-  },
-  {
-    id: 1,
-    name: "30 proyectos 30 días.",
-    description:
-      "Repositorio de Github. 30 proyectos en 30 días, uno por dia. ¡No puedo dejar pasar ni uno solo!",
-    link: "https://yefreescoding.github.io/30-projects-30-days/",
-    github: "https://github.com/yefreescoding/30-projects-30-days.git",
-    imagesrc: "/images/projects-img/30-30.jpg",
-    video: false,
-  },
-  {
-    id: 2,
-    name: "Music-gram",
-    description:
-      "Una pagina web donde poder ver los artistas del momento, sus ultimas canciones, colaboraciones, etc.",
-    link: "https://music-gram-yefreescoding.vercel.app",
-    github: "https://github.com/yefreescoding/music-gram.git",
-    imagesrc: "/images/projects-img/musicgram.jpg",
-    video: false,
-  },
-  {
-    id: 5,
-    name: "Boxing Match",
-    description:
-      "Noticias del boxeo, las mejores peleas, los mejores boxeadores, la información que te interesa cortita y al pie. ",
-    link: "https://proyecto-final-colon.000webhostapp.com/index.php",
-    github: "https://github.com/yefreescoding/final-project-course",
-    imagesrc: "/images/projects-img/boxing.jpg",
-    video: false,
-  },
-  {
-    id: 6,
-    name: "Nike Lite",
-    description:
-      "Landing page para la pagina oficial de Nike, pero una version mas optimizada.",
-    link: "https://nike-lite.vercel.app",
-    github: "https://github.com/yefreescoding/nike-lite.git",
-    imagesrc: "/images/projects-img/nike-lite.jpg",
-    video: false,
+      "React + typeScript application, no framework was used in the development of this website.",
+    link: "/memorias/",
+    github: "/memorias/",
+    imagesrc: "/images/projects/video/mail-component.webm",
+    video: true,
   },
 ];
