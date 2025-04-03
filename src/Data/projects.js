ae570d8ca379085d1eb99081f867b9d1c2af452d
Yefree Valdez <yefree13santos@outlook.com>
2025-04-03 18:21:14 -0400

    upload new projects and assets

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -21,12 +21,21 @@ export const projects = [
   },
   {
     id: 3,
-    name: "Overthink v2",
+    name: "GNX Landing Page",
     description:
-      "React + typeScript application, no framework was used in the development of this website.",
+      "Landing page for a release album. No framework was used in the development of this website.",
     link: "/memorias/",
     github: "/memorias/",
-    imagesrc: "/images/projects/video/overthink-v2.webm",
+    imagesrc: "/images/projects/video/gnx-landing-page.webm",
+    video: true,
+  },
+  {
+    id: 3007,
+    name: "Nice Hover Effect",
+    description: "Description",
+    link: "/memorias/",
+    github: "/memorias/",
+    imagesrc: "/images/projects/video/article-card-video.webm",
     video: true,
   },
   {
@@ -47,6 +56,16 @@ export const projects = [
     link: "/memorias/",
     github: "/memorias/",
     imagesrc: "/images/projects/video/scroll-react.webm",
+    video: true,
+  },
+  {
+    id: 79807,
+    name: "Cool Social Card.",
+    description:
+      "React + typeScript application, no framework was used in the development of this website.",
+    link: "/memorias/",
+    github: "/memorias/",
+    imagesrc: "/images/projects/video/cool-social-card.webm",
     video: true,
   },
   {
---
+++ src/MockComponents/MockMenu.tsx
@@ -0,0 +1,3 @@
+export const MockMenu = () => {
+  return <div>MockMenu</div>;
+};
